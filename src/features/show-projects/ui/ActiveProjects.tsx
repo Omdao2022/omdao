9e278ec7df02c1607017615a29b36211af0e200c
AlexDv19 <alexdav75149@gmail.com>
2023-11-29 08:45:40 +0300

    close the project to Kraken

--- src/features/show-projects/ui/ActiveProjects.tsx
+++ src/features/show-projects/ui/ActiveProjects.tsx
@@ -4,7 +4,6 @@ import { IProjectProps, Project } from "./Project";
 import { useTranslation } from "react-i18next";
 import {
     COMMON_AG_DATA,
-    COMMON_KRK_DATA,
     COMMON_QNod_DATA,
     COMMON_CRB_DATA,
 } from "../../../shared/constants/blockchain";
@@ -18,12 +17,7 @@ const getActiveProjects = (t: TFunction) => {
             href: COMMON_AG_DATA.herf,
             buttonName: t("common.swap") + " omAra /",
         },
-        {
-            title: COMMON_KRK_DATA.title,
-            symbol: COMMON_KRK_DATA.symbol,
-            href: COMMON_KRK_DATA.herf,
-            buttonName: t("common.swap"),
-        },
+        
         {
             title: COMMON_QNod_DATA.title,
             symbol: COMMON_QNod_DATA.symbol,
